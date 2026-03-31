3533262cd6b36638b3d8535d7c8c1603a89baeed
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-03-31 09:03:45 +0700

    menambahkan int

--- Tugas_Looping.cpp
+++ Tugas_Looping.cpp
@@ -14,7 +14,7 @@ int pilihan = 0;
 //Function untuk mengecek bilangan prima menggunakan while loop
 bool isPrima(int n){
     if(n <= 1) return false;
-    if(n == 2 n == 3) return true;
+    if(n == 2 || n == 3) return true;
 
     int i = 2;
     while(i * i <= n){
@@ -29,5 +29,8 @@ bool isPrima(int n){
 // Function untuk mengecek bilangan Fibonacci menggunakan while loop
 bool isFibonacci(int n){
     if(n < 0) return false;
+    if(n == 0 || n == 1) return true;
+
+    int a = 0,b = 1,c;
 
 }
